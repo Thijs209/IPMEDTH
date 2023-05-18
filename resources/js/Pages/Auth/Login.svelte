fb568070973bec0fb84158baf4b30472ff2e3056
jshas <62602628+jshas@users.noreply.github.com>
2023-05-19 00:51:04 +0200

    Barebones form and homepage added

--- resources/js/Pages/Auth/Login.svelte
+++ resources/js/Pages/Auth/Login.svelte
@@ -16,20 +16,135 @@
     }
 </script>
 
-<form on:submit|preventDefault={submit}>
-    <input type="text" bind:value={$form.email} />
-    {#if $form.errors.email}
-        <div class="form-error">{$form.errors.email}</div>
-    {/if}
-    <input type="password" bind:value={$form.password} />
-    {#if $form.errors.password}
-        <div class="form-error">{$form.errors.password}</div>
-    {/if}
-    <input type="checkbox" bind:checked={$form.remember} /> Remember Me
-    <button type="submit" disabled={$form.processing}>Submit</button>
-</form>
-*
+<main class="page">
+    <article class="login">
+        <h2 class="login__heading">Login Page</h2>
+        <form class="form" on:submit|preventDefault={submit}>
+            <div class="form__group">
+                <label class="form__label" for="email">Email</label>
+                <input
+                    class="form__input"
+                    type="text"
+                    bind:value={$form.email}
+                />
+                {#if $form.errors.email}
+                    <div class="form-error">{$form.errors.email}</div>
+                {/if}
+            </div>
+
+            <div class="form__group">
+                <label class="form__label" for="password">Password</label>
+                <input
+                    class="form__input"
+                    type="password"
+                    bind:value={$form.password}
+                />
+                {#if $form.errors.password}
+                    <div class="form-error">{$form.errors.password}</div>
+                {/if}
+            </div>
+            <div class="form__group form__group--checkbox">
+                <label class="form__label" for="remember">Remember me</label>
+                <input
+                    class="form__checkbox"
+                    type="checkbox"
+                    bind:checked={$form.remember}
+                />
+            </div>
+            <button
+                class="form__button"
+                type="submit"
+                disabled={$form.processing}>Login</button
+            >
+        </form>
+    </article>
+</main>
 
 <style>
-    /* your styles go here */
+    .page {
+        height: 100vh;
+        width: 100vw;
+        background-color: #eee;
+        display: flex;
+
+        justify-content: center;
+        align-items: center;
+    }
+
+    .login {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+
+        min-width: 30rem;
+        gap: 2rem;
+        padding: 2rem;
+        background-color: #fff;
+        justify-self: center;
+        align-self: center;
+    }
+    .form {
+        height: auto;
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .form__group {
+        display: flex;
+        flex-flow: column wrap;
+        width: 100%;
+        align-self: center;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 1rem;
+    }
+
+    .form__group--checkbox {
+        align-self: flex-start;
+        flex-flow: row wrap;
+        gap: 2rem;
+    }
+
+    .form__label {
+        display: flex;
+        height: 2rem;
+        align-self: flex-start;
+        font-size: 1.5rem;
+        font-weight: 700;
+        margin-bottom: 0.5rem;
+    }
+
+    .form__input {
+        width: 100%;
+        height: 3rem;
+        border-radius: 0.5rem;
+        border: 1px solid #ccc;
+        padding: 0 1rem;
+        font-size: 1.5rem;
+    }
+
+    .form__checkbox {
+        width: 1.5rem;
+        height: 1.5rem;
+        align-self: start;
+    }
+    .form__button {
+        width: 100%;
+        align-self: center;
+        height: 3rem;
+        border-radius: 0.5rem;
+        border: 1px solid #ccc;
+        padding: 0 1rem;
+        font-size: 1.5rem;
+        background-color: var(--c-primary-button);
+        color: #fff;
+    }
+
+    .form__button:hover {
+        background-color: var(--c-primary-button-hover);
+        cursor: pointer;
+    }
 </style>
--- resources/js/Pages/Home.svelte
+++ resources/js/Pages/Home.svelte
@@ -1,18 +1,143 @@
 <script>
-    import { inertia, page } from "@inertiajs/svelte";
+    import { inertia, Link, page } from "@inertiajs/svelte";
     let user = "World"; //
     console.log($page.props.auth.user);
 </script>
 
-<div>
-    {#if $page.props.auth.user}
-        <h1>
-            Welcome! You're signed in as {$page.props.auth.user.first_name}!
-        </h1>
-    {:else}
-        <h1>Welcome! You're not signed in.</h1>
-    {/if}
+<header class="header">
+    <h1 class="header__heading">Home</h1>
+    <nav class="header__nav">
+        <ul class="header__items">
+            <li class="header__item">
+                <Link class="header__link" href="/">Home</Link>
+            </li>
 
-    <button use:inertia="{{ href: '/logout', method: 'post' }}" type="button">Logout</button>
+            <li class="header__item">
+                <Link class="header__link" href="/">Contact</Link>
+            </li>
+        </ul>
+    </nav>
+</header>
+{#if $page.props.auth.user}
+    <h2>
+        Welcome! You're signed in as {$page.props.auth.user.first_name}!
+    </h2>
+    <button
+        class="header__link header__link--logout"
+        use:inertia={{ href: "/logout", method: "post" }}
+        type="button">Logout</button
+    >
+{:else}
+    <h2>Welcome! You're not signed in.</h2>
+    <div class="buttons">
+        <button
+            class="button"
+            use:inertia={{ href: "/login", method: "get" }}
+            type="button">Login</button
+        >
+        <button
+            class="button"
+            use:inertia={{ href: "/register", method: "get" }}
+            type="button">Register</button
+        >
+    </div>
+{/if}
 
-</div>
+<style>
+    .header {
+        height: 20%;
+        width: 100%;
+        background-color: var(--c-nav-bg);
+        color: var(--c-nav-font);
+    }
+
+    .header__heading {
+        font-size: 2rem;
+        font-weight: 700;
+        margin-bottom: 1rem;
+    }
+
+    .header__nav {
+        display: flex;
+
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .header__items {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .header__item {
+        list-style: none;
+        margin-right: 1rem;
+    }
+
+    .header__link {
+        z-index: 2;
+        text-decoration: none;
+        color: var(--c-nav-font);
+        font-size: 1.2rem;
+        font-weight: 700;
+    }
+
+    .header__link:visited {
+        color: var(--nav-font);
+    }
+
+    .header__link--login {
+        background-color: var(--arcady-green);
+        color: #fff;
+        padding: 0.5rem 1rem;
+        border-radius: 0.5rem;
+    }
+
+    .header__link--logout {
+        background-color: #000;
+        color: #fff;
+        padding: 0.5rem 1rem;
+        border-radius: 0.5rem;
+        border: none;
+    }
+
+    .header__link--logout:hover {
+        background-color: #fff;
+        color: var(--c-alert-hover);
+        cursor: pointer;
+    }
+
+    .header__link--login:hover {
+        background-color: #fff;
+        color: #000;
+    }
+
+    .header__link:hover {
+        color: #fff;
+        background-color: #000;
+    }
+
+    .buttons {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .button {
+        width: 100%;
+        align-self: center;
+        height: 3rem;
+        border-radius: 0.5rem;
+        border: 1px solid #ccc;
+        padding: 0 1rem;
+        font-size: 1.5rem;
+        background-color: var(--c-primary-button);
+        color: #fff;
+    }
+    .button:hover {
+        background-color: var(--c-primary-button-hover);
+        color: #fff;
+        cursor: pointer;
+    }
+</style>
