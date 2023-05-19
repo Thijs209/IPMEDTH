187c2975438fb01be580b0695d2038a20b912802
jshas <62602628+jshas@users.noreply.github.com>
2023-05-19 20:49:08 +0200

    Fixed missing function declaration

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -7,6 +7,17 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array<string, string>  $input
+     */
+    public function create(array $input): User
+    {
         Validator::make($input, [
             'first_name' => ['required', 'string', 'max:40'],
             'last_name' => ['required', 'string', 'max:40'],
