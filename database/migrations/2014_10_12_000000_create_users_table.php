934d959c3f448456bc218459a9cb57e7ff54e598
jshas <62602628+jshas@users.noreply.github.com>
2023-05-25 14:48:23 +0200

    Fix: role_id foreign key issue (can't be NOT NULL)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('role_id')->constrained('roles')->nullOnDelete()->onUpdate('cascade');
+            $table->foreignId('role_id')->constrained()->cascadeOnDelete()->restrictOnDelete();
             $table->rememberToken();
             $table->timestamps();
         });
