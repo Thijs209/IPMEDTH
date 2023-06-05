8d139166be7608131360b4da7a229308ae931bda
jshas <62602628+jshas@users.noreply.github.com>
2023-06-05 23:05:02 +0200

    Made  /login and /password.reset named routes -- Removed comments from NavButton.svelte

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,16 +21,23 @@ Route::get('/', function () {
 
 Route::get('/login', function () {
     return Inertia::render('Auth/Login');
-});
+})->name('login');
 
 Route::get('/register', function () {
     return Inertia::render('Auth/Register');
-});
+})->name('register');
 
 Route::get('/forgot-password', function () {
     return Inertia::render('Auth/ForgotPassword');
 });
-
 Route::get('/success', function () {
     return Inertia::render('Auth/Success');
-});
+})->name('auth.success');
+
+
+Route::get('/reset-password/{token}', function ($request) {         
+    Log::info($request);
+    return Inertia::render('Auth/ResetPassword', [
+        'request' => $request
+    ]);
+})->name('password.reset');
