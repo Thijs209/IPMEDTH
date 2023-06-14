8c95f8e40e628ea61cdbc521a94d4cc53b2851ce
Vyash Bhawan <vyashbhawan@dhcp-145.136.225.32.pool.hsleiden.nl>
2023-06-14 16:15:11 +0200

    added evaluation store function

---
+++ app/Http/Controllers/EvaluationNoteController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EvaluationNote;
+use Illuminate\Http\Request;
+use App\Http\Resources\EvaluationNoteResource;
+
+class EvaluationNoteController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'pop_id' => 'bail|required',
+        ]);
+
+        $note = new EvaluationNote();
+        $note->pop_id = $request['pop_id'];
+        $note->evaluation_note_type_id = $request['note_type_id'];
+        $note->evaluation_note = $request['note'];
+
+        $note->save();
+
+        return EvaluationNoteResource::make($note);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EvaluationNote $evaluationNote)
+    {
+        return EvaluationNoteResource::make($evaluationNote);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EvaluationNote $evaluationNote)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EvaluationNote $evaluationNote)
+    {
+        //
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             UserSeeder::class,
             PopSeeder::class,
-            GoalTypeSeeder::class
+            GoalTypeSeeder::class,
+            EvaluationNoteTypeSeeder::class
         ]);
     }
 }
