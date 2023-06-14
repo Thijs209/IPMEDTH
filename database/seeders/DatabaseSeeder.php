1e103257554473f08747db26a6ca5f519dc9d9cb
jshas <62602628+jshas@users.noreply.github.com>
2023-06-14 23:01:48 +0200

    Added POP seeder and references

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
+            PopSeeder::class,
         ]);
     }
 }
--- database/seeders/PopSeeder.php
+++ database/seeders/PopSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use DB;
 
 class PopSeeder extends Seeder
 {
@@ -12,6 +13,9 @@ class PopSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+       
+     DB::table('pops')->insert([
+            'user_id' => 4,
+     ]);
     }
 }
