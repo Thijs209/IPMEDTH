b96d1bb721a05f3773e598fab7862aad6dbe966c
jshas <62602628+jshas@users.noreply.github.com>
2023-06-14 23:47:38 +0200

    Added EvaluatePopButtons (styling)

---
+++ resources/js/Components/EvaluatePopComponents/EvaluatePopButtons.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    // TODO functie schrijven om de data op te slaan in de database bij het klikken op de opslaan knop
+</script>
+
+<div class="evaluate-pop__buttons">
+    <button class="button button--close">
+        <p>Sluiten</p>
+    </button>
+    <button class="button">
+        <p>Opslaan</p>
+    </button>
+</div>
+
+<style>
+    .evaluate-pop__buttons {
+        display: flex;
+        flex-flow: row nowrap;
+        gap: 2rem;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+    }
+    button {
+        background-color: var(--c-primary-button);
+        font-family: "Montserrat", sans-serif;
+        font-weight: bold;
+        font-size: 1.7rem;
+        min-height: 6rem;
+        min-width: 15rem;
+        height: 100%;
+        width: 45%;
+        max-height: 10rem;
+        max-width: 20rem;
+        border: none;
+        color: #eee;
+        border-radius: 1rem;
+    }
+
+    .button--close {
+        background-color: var(--c-secondary-button);
+    }
+
+    .button--close:hover {
+        background-color: var(--c-secondary-button-hover);
+    }
+
+    button:hover {
+        background-color: var(--c-primary-button-hover);
+        cursor: pointer;
+    }
+</style>
