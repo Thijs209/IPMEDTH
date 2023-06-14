6b4fb7110a0d2234cad9b7c77a23d188e18f69cf
jshas <62602628+jshas@users.noreply.github.com>
2023-06-14 23:46:03 +0200

    Added POP notes components setup and position

---
+++ resources/js/Components/EvaluatePopComponents/PopNote.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+    export let date: string;
+    export let time: string;
+    export let note: string;
+</script>
+
+<section class="container">
+    <div class="date">
+        <p class="date__time">
+            {time + " uur"}
+        </p>
+        <p class="date__date">
+            {"1 feb. 2021"}
+        </p>
+    </div>
+    <div class="note">
+        <p>
+            {note
+                ? note
+                : "Lege note."}
+        </p>
+    </div>
+</section>
+
+<style>
+    .container {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-between;
+        font-family: "Montserrat", sans-serif;
+        width: 90%;
+        margin: 0 auto;
+        min-height: 10rem;
+        padding: 2rem;
+        gap: 1rem;
+        border-bottom: #ddd 0.4rem solid;
+        align-self: start;
+    }
+    .date {
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: space;
+        align-items: end;
+        width: 20%;
+        height: 100%;
+        background-color: pink;
+        text-align: right;
+    }
+
+    .date__time {
+        font-weight: 600;
+        color: #222;
+        font-size: 1.3rem;
+    }
+
+    .date__date {
+        font-style: italic;
+        color: #222;
+        font-size: 1.2rem;
+    }
+
+    .note {
+        width: 80%;
+        height: 100%;
+        color: black;
+        font-size: 1.3rem;
+    }
+</style>
---
+++ resources/js/Components/EvaluatePopComponents/PopNotes.svelte
@@ -0,0 +1,125 @@
+<script lang="ts">
+    import { writable } from "svelte/store";
+    import { currentPopNotes } from "./../../stores.js";
+    import { useForm, router, page } from "@inertiajs/svelte";
+    import { MdSave } from "svelte-icons/md";
+    import moment from "moment";
+    import "moment/locale/nl";
+    import IconHolder from "./../IconHolder.svelte";
+    import PopNote from "./PopNote.svelte";
+    import CoreQuadrant from "./CoreQuadrant.svelte";
+
+    type Note = {
+        date: string;
+        time: string;
+        note: string;
+    };
+    // TODO LocalStorage voor notes uitwerken
+
+    export let notes: Note[] = [
+        {
+            date: moment("20-01-2022", "DD-MM-YYYY").format(),
+            time: moment(2000).format("HH:mm"),
+            note: "Yuhyuhyuh",
+        },
+        {
+            date: moment("22-01-2022", "DD-MM-YYYY").format(),
+            time: moment(2011).format("HH:mm"),
+            note: "",
+        },
+    ];
+    console.log(notes);
+
+    let form = useForm({
+        date: moment().format("DD-MM-YYYY"),
+        time: moment().format("HH:mm"),
+        note: "",
+    });
+
+    currentPopNotes.subscribe((value) => {
+        console.log(value);
+    });
+    let newNote: (Note) = {
+            date: $form.date,
+            time: $form.time,
+            note: $form.note};
+    const handleSave = () => {
+    };
+
+    $: $form, console.log($currentPopNotes);
+</script>
+
+<h2 class="notes__header">Interne notities</h2>
+<section class="notes">
+    <section class="notes__list">
+        {#each notes as note}
+            <div class="note">
+                <PopNote date={note.date} time={note.time} note={note.note} />
+            </div>
+        {/each}
+    </section>
+    <form class="notes__form">
+        <input id="note" name="note" type="text" bind:value={$form.note} />
+        <button
+            on:keydown|preventDefault
+            on:submit|preventDefault={() => handleSave()}
+        >
+            <IconHolder>
+                <MdSave />
+            </IconHolder>
+        </button>
+    </form>
+</section>
+
+<style>
+    .notes {
+        display: flex;
+        flex-flow: column wrap;
+        background-color: #fff;
+        height: 100%;
+        width: 100%;
+        min-width: 100%;
+        font-family: "Montserrat", sans-serif;
+        border-radius: 1.5rem;
+    }
+
+    .notes__list {
+        height: 90%;
+        max-height: 90%;
+        min-width: 100%;
+        width: 100%;
+        overflow: scroll;
+    }
+
+    .notes__header {
+        font-size: 1.8rem;
+    }
+
+    .notes__form {
+        display: flex;
+        flex-flow: row nowrap;
+        gap: 0.1rem;
+        justify-content: center;
+        align-items: center;
+        margin: 0 auto;
+        width: 100%;
+        margin-top: auto;
+        margin-bottom: 1rem;
+        box-shadow: inset;
+    }
+
+    .notes__form > input {
+        height: 4rem;
+        width: 80%;
+        gap: 1rem;
+    }
+
+    .notes__form > button {
+        background-color: white;
+
+        border: 1px solid black;
+        height: 4rem;
+        /* min-height: 100%; */
+        width: 10%;
+    }
+</style>
